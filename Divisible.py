46de9244b2a7c86719c842270929d954fba971be
AKASH2082001 <akashbme208@gmail.com>
2022-03-25 00:49:59 -0700

    Automations

--- Divisible.py
+++ Divisible.py
@@ -1,20 +1,20 @@
 import unittest
 
 def Divisibleby7(x):
-    if x%7 == 0:
+    if x % 7 == 0:
         return True
     else:
         return False
     
 class Checkdivisible1(unittest.TestCase):
     
-    def test_divisibleby7_1(self):
+    def test_case_divisibleby7_1(self):
         result = Divisibleby7(14)
         self.assertTrue(result)
         
-    def test_divisibleby7_2(self):
+    def test_case_divisibleby7_2(self):
         result = Divisibleby7(4)
         self.assertFalse(result)
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     unittest.main()
